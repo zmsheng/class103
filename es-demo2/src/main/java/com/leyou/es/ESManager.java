17284efe2c49b6fdb0b9c5376419f0bf27c0e4ea
zmsheng <zmsheng@123.com>
2019-09-18 21:49:37 +0800

    hy

--- es-demo2/src/main/java/com/leyou/es/ESManager.java
+++ es-demo2/src/main/java/com/leyou/es/ESManager.java
@@ -195,14 +195,6 @@ public class ESManager {
     @After
     public void end() throws Exception{
         client.close();
-
-		 System.out.println("张三");
-    }
-	@Test
-
-    public void testgithubLS() throws Exception{
-		 System.out.println("李四");
-		 System.out.println("张三？？");
     }
 
 }
